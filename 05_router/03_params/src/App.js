651f3af098148e1500a24538b1b229f860c12e9b
LEEMINKUK <cjcjcj963@gmail.com>
2024-03-19 14:47:35 +0900

    commit

--- 05_router/03_params/src/App.js
+++ 05_router/03_params/src/App.js
@@ -4,6 +4,7 @@ import Main from "./pages/Main";
 import About from "./pages/About";
 import Menu from "./pages/Menu";
 import MenuDetails from "./pages/MenuDetails";
+import MenuSearchResult from "./pages/MenuSearchResult";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="menu">
               <Route index element={<Menu/>}/>
               <Route path=":menuCode" element={<MenuDetails/>}/>
+              <Route path="search" element={<MenuSearchResult/>}/>
             </Route>
           </Route>
         </Routes>
