ca62777cad03ff61aad27c22fc61ca696b69f13e
LEEMINKUK <cjcjcj963@gmail.com>
2024-03-20 17:27:13 +0900

    commit

---
+++ 05_router/04_etc/src/App.js
@@ -0,0 +1,27 @@
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import MyPage from './pages/MyPage';
+import Login from './pages/Login';
+import Layout from './layouts/Layout'
+import Main from './pages/Main';
+import Error from './pages/Error';
+
+function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Main/>}/>
+          <Route path="main" element={<Main/>}/>
+          <Route path="mypage" element={<MyPage/>}/>
+          <Route path="login" element={<Login/>}/>
+        </Route>
+
+      <Route path='*' element={<Error/>}/> {/* 404 에러*/}
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ 05_router/04_etc/src/components/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+
+    return(
+        <h1>Welcom to Ohgiraffers Restaurant</h1>
+    )
+}
+
+export default Header;
